5192c6ee11ea42842ab8b5460faf3ef7242b7c59
bharatupes-code <bharatupes@gmail.com>
2026-04-11 22:58:06 +0530

    day50.c

---
+++ day50.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Node structure
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+};
+
+// Create node
+struct Node *createNode(int data)
+{
+    struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+// Insert into BST
+struct Node *insert(struct Node *root, int data)
+{
+    if (root == NULL)
+        return createNode(data);
+
+    if (data < root->data)
+        root->left = insert(root->left, data);
+    else
+        root->right = insert(root->right, data);
+
+    return root;
+}
+
+// Search in BST
+int search(struct Node *root, int key)
+{
+    if (root == NULL)
+        return 0;
+
+    if (root->data == key)
+        return 1;
+
+    if (key < root->data)
+        return search(root->left, key);
+    else
+        return search(root->right, key);
+}
+
+// Main
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    struct Node *root = NULL;
+    int x;
+
+    // Insert elements
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &x);
+        root = insert(root, x);
+    }
+
+    int key;
+    scanf("%d", &key);
+
+    if (search(root, key))
+        printf("Found");
+    else
+        printf("Not Found");
+
+    return 0;
+}
